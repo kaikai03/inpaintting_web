133a33bc526513102e614d7ac9f6703da3b9f078
cains <l6751902@163.com>
2020-04-21 11:38:47 +0800

    add main layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>{{msg}},{{dirname}}</p>
-      <el-button @click="startHacking">Start</el-button>
-      <el-input v-model="msg"></el-input>
-    </div>
-    <firstcomponent></firstcomponent>
-    <ul>
-      <li> <router-link to="/first" >“第一页” </router-link> </li>
-      <li> <router-link to="/second" >“第二页” </router-link> </li>
-      <!-- 带查询参数，下面的结果为 /second?xxx=private -->
-      <li> <router-link to="/third" >“third” </router-link> </li>
-    </ul>
     <transition mode="out-in">
-      <router-view name="view1"></router-view>
-    </transition>
-    <transition mode="out-in">
-      <router-view name="view2"></router-view>
+      <mainlayout></mainlayout>
     </transition>
   </div>
 </template>
 
 <script>
-  import firstcomponent from "~/components/firstcomponent";
+  import mainlayout from "~/components/mainlayout";
   export default {
     data(){
       return {
@@ -32,40 +16,17 @@
       }
     },
     methods: {
-      startHacking () {
-        this.$notify({
-          title: 'It works!',
-          type: 'success',
-          message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-          duration: 5000
-        })
-      }
+
     },
     components:{
-      firstcomponent
+      mainlayout
     }
   }
 </script>
 
 <style>
-  #app {
-    font-family: Helvetica, sans-serif;
-    text-align: center;
-  }
-  .router-link-active{
-    background-color: beige;
-    font-size: 30px;
-  }
-
-  .v-enter,
-  .v-leave-to {
-    opacity: 0;
-    transform: translateX(140px);
-  }
-
-  .v-enter-active,
-  .v-leave-active {
-    transition: all 0.5s ease
-  }
+#app{
+  height: 100%;
+}
 
 </style>
---
+++ src/components/mainlayout.vue
@@ -0,0 +1,83 @@
+<template>
+  <el-container>
+    <!-- 顶栏 -->
+    <el-header height="60px">
+      <h2>Element 页面布局</h2>
+    </el-header>
+    <!-- 嵌套容器 -->
+    <el-container>
+      <!-- 侧边导航菜单 -->
+      <el-aside width="150px"></el-aside>
+      <!-- 内容 -->
+      <el-main>
+        <!-- 第一列栅格布局 -->
+        <el-row>
+          <el-col :span="12" class="col1">
+            <li>
+              <router-link to="/first">“第一页”</router-link>
+            </li>
+          </el-col>
+          <el-col :span="12" class="col2">
+            <li>
+              <router-link to="/third">“third”</router-link>
+            </li>
+          </el-col>
+        </el-row>
+        <!-- 第二列布局 -->
+        <el-row>
+          <el-col :span="24" class="col3">
+            <router-view name="view1"></router-view>
+            <router-view name="view2"></router-view>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+    <!-- 底栏 -->
+    <el-footer height="30px">&copy;究极死胖兽 2019</el-footer>
+  </el-container>
+</template>
+
+<script>
+    import firstcomponent from "~/components/firstcomponent";
+    import secondcomponent from "~/components/secondcomponent";
+    import third from "~/components/third";
+    export default {
+        name: "mainlayout"
+    }
+</script>
+
+
+<style>
+  .el-container {
+    min-height:800px; height:auto!important; height:900px;
+  }
+.el-header {
+  background-color: #409EFF;
+  color: white;
+}
+.el-footer {
+  background-color: #909399;
+  color: black;
+  text-align: center;
+}
+.el-aside {
+  background-color: chartreuse;
+}
+.el-main {
+  background-color: darkkhaki;
+}
+.el-col {
+  height: 200px;
+}
+.col1 {
+  background-color: teal;
+}
+.col2 {
+  background-color: tomato;
+}
+.col3 {
+  background-color: thistle;
+}
+</style>
+
+
