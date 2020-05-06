a2db3f96b0fac45cda86f0562301fd4610760ef5
cairn <kai_kai03@hotmail.com>
2020-05-06 18:39:00 +0800

    ~

--- src/components/common/img_loading_progress.vue
+++ src/components/common/img_loading_progress.vue
@@ -10,7 +10,7 @@
               count:0,
               playitems:[{"index":1,"type":"0","img":"http://127.0.0.1:90/imgs/1.jpg","media":"http://127.0.0.1:8020/mv/mv1.mp4"},
                 {"index":2,"type":"1","img":"http://127.0.0.1:90/imgs/2.jpg","media":"http://127.0.0.1:8020/gif/gif1.gif"},
-                {"index":3,"type":"1","img":"http://127.0.0.1:90/imgs/3.png","media":"http://127.0.0.1:8020/gif/gif3.gif"}]
+                {"index":3,"type":"1","img":"http://127.0.0.1:90/imgs/3.gif","media":"http://127.0.0.1:8020/gif/gif3.gif"}]
           }
         },
         mounted() {
@@ -20,6 +20,7 @@
                 img.src = item.img
                 img.onload = (e)=>{
                     if(this.count == this.playitems.length-1){
+                        console.log("加载OK： ",this.count+' : '+e.target.src)
                         console.log("全部完成")
                         // this.$router.push({name:"third3"})
                     }else{
