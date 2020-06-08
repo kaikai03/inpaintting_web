de0af6a69d9a4d562d43f21844bc3c3fc3746fbc
kai_kai03 <kai_kai03@hotmail.com>
2020-06-09 01:11:51 +0800

    add the upload limit

--- src/components/uploadtask.vue
+++ src/components/uploadtask.vue
@@ -4,18 +4,19 @@
             class="upload-file"
             ref="uploadimg"
             :action=this.backen.upload_img_urlmaker()
-            :on-preview="handlePreview"
-            :on-remove="handleRemove"
-            :before-remove="beforeRemove"
-            :on-error="handleError"
-            :on-success="handleSuccess"
+            :before-upload="before_upload"
+            :on-preview="handle_preview"
+            :on-remove="handle_remove"
+            :before-remove="before_remove"
+            :on-error="handle_error"
+            :on-success="handle_success"
             multiple
-            :limit="3"
-            :on-exceed="handleExceed"
+            :limit="10"
+            :on-exceed="handle_exceed"
             :file-list="fileList"
             list-type="picture">
-        <el-button size="small" type="primary">点击上传</el-button>
-        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        <el-button size="small" type="primary"><i class="el-icon-upload"></i></el-button>
+        <div slot="tip" class="el-upload__tip">支持jpg/png/bmp，最大5M</div>
     </el-upload>
 
 </template>
@@ -26,30 +27,49 @@
         data() {
             return {
                 // [{name,url}]
-                fileList: []
+                fileList: [],
+                limit_flag:false
             };
         },
         methods: {
-            handleSuccess(response, file, fileList) {
+            before_upload(file) {
+                const is_in_condition = (file.type === 'image/jpeg') || (file.type === 'image/png') || (file.type === 'image/bmp');
+                const is_in_5M = file.size / 1024 / 1024 < 5;
+                if (!is_in_condition) {
+                    this.$message.error('上传图片格式非法，请注意限制!');
+                    this.limit_flag = true
+                }
+                if (!is_in_5M) {
+                    this.$message.error('上传图片大小不能超过 5MB!');
+                    this.limit_flag = true
+                }
+                return is_in_condition && is_in_5M;
+            },
+            handle_success(response, file, fileList) {
                 // fileList.push({'name':file.response.origin, 'remote_name':file.response.save_name})
                 console.log('handleSuccess:',file);
             },
-            handleError(err, file, fileList) {
+            handle_error(err, file, fileList) {
                 console.log('handleError:',err);
             },
-            handlePreview(file) {
+            handle_preview(file) {
                 console.log(this.$refs.uploadimg.uploadFiles);
                 console.log('handlePreview:', file);
             },
-            handleExceed(files, fileList) {
-                this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+            handle_exceed(files, fileList) {
+                this.$message.warning(`当前限制选择 10 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
             },
-            beforeRemove(file, fileList) {
+            before_remove(file, fileList) {
+                if(this.limit_flag){
+                    this.limit_flag = false
+                    return true
+                }
                 return this.$confirm(`确定移除 ${file.name}？`);
             },
-            handleRemove(file, fileList) {
+            handle_remove(file, fileList) {
                 console.log('handleRemove:', file);
             }
+
         }
     }
 </script>
