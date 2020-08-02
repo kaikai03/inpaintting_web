9d6e62a62f4111f1d28d5e5cc50fc7a453c15e5d
kai_kai03 <kai_kai03@hotmail.com>
2020-08-02 23:57:42 +0800

    infinite-list

--- src/components/taskqueue.vue
+++ src/components/taskqueue.vue
@@ -1,16 +1,40 @@
 <template>
     <div class="taskqueue">
-        <h1>taskqueue</h1>
-        <h5>{{author}}</h5>
+        <div class="infinite-list" style="overflow:auto" >
+            <ul class="list" v-infinite-scroll="load" infinite-scroll-disabled="disabled">
+                <li v-for="i in count" class="list-item">{{ i }}</li>
+            </ul>
+            <p v-if="loading">加载中...</p>
+            <p v-if="noMore">没有更多了</p>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "taskqueue",
-        data(){
+        data() {
             return {
-                author:this.$route.path
+                count: 10,
+                loading: false
+            }
+        },
+        computed: {
+            noMore() {
+                return this.count >= 202
+            },
+            disabled() {
+                return this.loading || this.noMore
+            }
+        },
+        methods: {
+            load() {
+                // this.loading = true
+                // setTimeout(() => {
+                //     this.count += 2
+                //     this.loading = false
+                // }, 2000)
+                this.count += 2
             }
         }
     }
@@ -19,7 +43,7 @@
 <style>
 .taskqueue {
     width: 100%;
-    height: 100%;
+    max-height: 100px;
   font-family: Helvetica, sans-serif;
   text-align: center;
 }
