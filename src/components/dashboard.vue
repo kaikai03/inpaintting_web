4a04c77095df80f78921ed308dc1916dee4e43af
kai_kai03 <kai_kai03@hotmail.com>
2020-09-15 00:10:38 +0800

    chart component import test

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
     <div>芜湖dash</div>
+        <lineChart></lineChart>
     </div>
 </template>
 
 <script>
+    import areasplineChart from "~/components/common/areasplineChart";
     export default {
         name: "dashboard",
-
+        components: {
+            lineChart:areasplineChart
+        }
     }
 </script>
 
