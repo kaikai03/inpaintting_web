cbfd6f53399eb5e2bd5016f30f36a9b9d5411075
kai_kai03 <kai_kai03@hotmail.com>
2020-10-11 23:32:54 +0800

    add tabs

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,21 +1,27 @@
 <template>
     <div>
-        <el-tabs :tab-position="top" style="height: 200px;">
-            <el-tab-pane label="用户管理">用户管理</el-tab-pane>
-            <el-tab-pane label="配置管理">配置管理</el-tab-pane>
-            <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-            <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+        <el-tabs :tab-position="top" style="height: 200px; width: 100%">
+            <el-tab-pane label="AAAA">
+                <div style="width: 300px;height: 200px;">
+                    <cpuChart ref="cpuChart" tag="cpuChart" title="CPU(%)" :lineCount=5
+                              :linesName="['CPU','CPU-1','CPU-2','CPU-3','CPU-4']"
+                              :linesShortName="['AVG','C_1','C_2','C_3','C_4']" unitSymbol="%">
+                    </cpuChart>
+                </div>
+            </el-tab-pane>
+            <el-tab-pane label="BBBB">
+                <div style="width: 300px;height: 200px;">
+                    <solidgaugeChart ref="memChart" tag="memChart" title="MEM" tipName="MEM" :min=0 :max=100
+                                     unitSymbol="%"></solidgaugeChart>
+                </div>
+            </el-tab-pane>
+
+            <el-tab-pane label="CCCC">角色管理</el-tab-pane>
+            <el-tab-pane label="DDDD">定时任务补偿</el-tab-pane>
         </el-tabs>
 
-        <div style="width: 300px;height: 200px;">
-            <cpuChart ref="cpuChart" tag="cpuChart" title="CPU(%)" :lineCount=5
-                      :linesName="['CPU','CPU-1','CPU-2','CPU-3','CPU-4']"
-                      :linesShortName="['AVG','C_1','C_2','C_3','C_4']" unitSymbol="%">
-            </cpuChart>
-        </div>
-        <div style="width: 300px;height: 200px;">
-            <solidgaugeChart ref="memChart" tag="memChart" title="MEM" tipName="MEM" :min=0 :max=100 unitSymbol="%"></solidgaugeChart>
-        </div>
+
+
         <span>
             <el-button type="primary" size="mini" class="submit" @click="updateBtn">update</el-button>
 
