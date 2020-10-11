4ed7d58409b7c6fc6a71b30d9dbf67ad44efe46f
kai_kai03 <kai_kai03@hotmail.com>
2020-10-12 00:56:46 +0800

    separate the chart to dashcontent.vue

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,53 +1,72 @@
 <template>
     <div>
-        <el-tabs :tab-position="top" style="height: 200px; width: 100%">
-            <el-tab-pane label="AAAA">
-                <div style="width: 300px;height: 200px;">
-                    <cpuChart ref="cpuChart" tag="cpuChart" title="CPU(%)" :lineCount=5
-                              :linesName="['CPU','CPU-1','CPU-2','CPU-3','CPU-4']"
-                              :linesShortName="['AVG','C_1','C_2','C_3','C_4']" unitSymbol="%">
-                    </cpuChart>
-                </div>
+        <div id="dashboard">
+        <div id="el-tab">
+        <el-tabs  tab-position="top" :v-model="activeTab" @tab-click="tagClick" :before-leave="tagLeave">
+            <el-tab-pane name="A">
+                <span slot="label"><i class="el-icon-date"></i> AAAA</span>
             </el-tab-pane>
-            <el-tab-pane label="BBBB">
-                <div style="width: 300px;height: 200px;">
-                    <solidgaugeChart ref="memChart" tag="memChart" title="MEM" tipName="MEM" :min=0 :max=100
-                                     unitSymbol="%"></solidgaugeChart>
-                </div>
+            <el-tab-pane label="BBBB" name="B">
+
             </el-tab-pane>
 
-            <el-tab-pane label="CCCC">角色管理</el-tab-pane>
-            <el-tab-pane label="DDDD">定时任务补偿</el-tab-pane>
+            <el-tab-pane label="CCCC" name="C">
+
+            </el-tab-pane>
+            <el-tab-pane label="DDDD" name="D"></el-tab-pane>
         </el-tabs>
-
-
-
-        <span>
-            <el-button type="primary" size="mini" class="submit" @click="updateBtn">update</el-button>
-
-        </span>
+        </div>
+        <div id="dashcontent">
+            <dashcontent ></dashcontent>
+        </div>
     </div>
+        </div>
 </template>
 
 <script>
-    import cpuChart from "~/components/common/multiLinesChart";
-    import solidgaugeChart from "~/components/common/solidgaugeChart";
+    import dashcontent from "~/components/dashcontent";
     export default {
         name: "dashboard",
+        data(){
+            return {
+                activeTab:null
+            }
+        },
         methods:{
-            updateBtn(){
-                this.$refs.cpuChart.updateLine("updateLine")
-                this.$refs.memChart.updateMem("updateMem")
-
+            tagClick(tab, event){
+                console.log("tagClick:",tab, event);
+            },
+            tagLeave(activeName, oldActiveName){
+                console.log("tagLeave:",activeName, oldActiveName);
             }
         },
         components: {
-            cpuChart:cpuChart, solidgaugeChart:solidgaugeChart
+            dashcontent:dashcontent,
         }
     }
 </script>
 
-<style scoped >
+<style scoped>
+    #dashboard{
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        width: 100%;
+    }
+    #el-tab{
+        background: blue;
+        height: 40px;
+        margin-top: 5px;
+        margin-left: 20px;
+        margin-right: 20px;
+    }
 
+    #dashcontent{
+        background: red;
+        height: calc(100% - 40px - 20px);
+        margin-top: 5px;
+        margin-left: 20px;
+        margin-right: 20px;
+    }
 
 </style>
---
+++ src/components/dashcontent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div style="width: 300px;height: 200px;">
+            <cpuChart ref="cpuChart" tag="cpuChart" title="CPU(%)" :lineCount=5
+                      :linesName="['CPU','CPU-1','CPU-2','CPU-3','CPU-4']"
+                      :linesShortName="['AVG','C_1','C_2','C_3','C_4']" unitSymbol="%">
+            </cpuChart>
+        </div>
+
+        <div style="width: 300px;height: 200px;">
+            <solidgaugeChart ref="memChart" tag="memChart" title="MEM" tipName="MEM" :min=0 :max=100
+                             unitSymbol="%"></solidgaugeChart>
+        </div>
+
+        <span>
+            <el-button type="primary" size="mini" class="submit" @click="updateBtn">update</el-button>
+
+        </span>
+    </div>
+</template>
+
+<script>
+    import cpuChart from "~/components/common/multiLinesChart";
+    import solidgaugeChart from "~/components/common/solidgaugeChart";
+
+    export default {
+        name: "dashcontent",
+        data(){
+            return {
+            }
+        },
+        methods:{
+            updateBtn(){
+                this.$refs.cpuChart.updateLine("updateLine")
+                // this.$refs.memChart.updateMem("updateMem")
+            },
+        },
+        components: {
+            cpuChart:cpuChart, solidgaugeChart:solidgaugeChart
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
