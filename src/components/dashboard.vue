aa3417dab224a73eb224e5c952c37986046baba4
kai_kai03 <kai_kai03@hotmail.com>
2020-10-11 23:30:33 +0800

    add tabs

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <el-tabs :tab-position="top" style="height: 200px;">
+            <el-tab-pane label="用户管理">用户管理</el-tab-pane>
+            <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+            <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+            <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+        </el-tabs>
+
         <div style="width: 300px;height: 200px;">
             <cpuChart ref="cpuChart" tag="cpuChart" title="CPU(%)" :lineCount=5
                       :linesName="['CPU','CPU-1','CPU-2','CPU-3','CPU-4']"
