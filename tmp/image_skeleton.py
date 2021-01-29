170742c19e45b4bc9ccaf053b4546658f938a25e
cairn <kai_kai03@hotmail.com>
2021-01-30 02:51:26 +0800

    ~

--- tmp/image_skeleton.py
+++ tmp/image_skeleton.py
@@ -2,12 +2,12 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
-img = Image.open("C:/Users/fakeQ/Desktop/电子签名2.png")
+img = Image.open("C:/Users/fakeQ/Desktop/2.png")
 img_grey = img.convert('L')
 img_a = img.convert('RGBA')
 
 mat_grey = np.asarray(img_grey)
-threshold = np.average(mat_grey)*0.6
+threshold = np.average(mat_grey)*0.8
 
 
 x, y = img.size
@@ -16,13 +16,13 @@ for i in range(x):
     for k in range(y):
         color = img_a.getpixel((i, k))
         if np.sum(color[:3])/3 > threshold:
-            color = color[:-1] + (0, )
+            color = color[:-1] + (25, )
         else:
             color = color[:-1] + (255,)
         img_a.putpixel((i, k), color)
 
 
-img_a.save("C:/Users/fakeQ/Desktop/电子签名2_deal.PNG")
+# img_a.save("C:/Users/fakeQ/Desktop/1_deal.PNG")
 
 plt.figure(12)
 plt.subplot(121)
