162f9032b8dcc225409d9cd7d2569edf0fe0290e
cairn <kai_kai03@hotmail.com>
2021-06-17 14:50:47 +0800

    ~

--- tmp/win.py
+++ tmp/win.py
@@ -2,62 +2,7 @@ import win32gui,win32con,win32api
 import win32clipboard as w
 from array import array
 import time
-import ctypes
-
-PBYTE256 = ctypes.c_ubyte * 256
-_user32 = ctypes.WinDLL("user32")
-GetKeyboardState = _user32.GetKeyboardState
-SetKeyboardState = _user32.SetKeyboardState
-PostMessage = win32api.PostMessage
-SendMessage = win32gui.SendMessage
-FindWindow = win32gui.FindWindow
-IsWindow = win32gui.IsWindow
-GetCurrentThreadId = win32api.GetCurrentThreadId
-GetWindowThreadProcessId = _user32.GetWindowThreadProcessId #it is very important to take the function from the dll, since Python wrapper (win32process.GetWindowThreadProcessId) gives incorrect values
-AttachThreadInput = _user32.AttachThreadInput
-
-MapVirtualKeyA = _user32.MapVirtualKeyA
-MapVirtualKeyW = _user32.MapVirtualKeyW
-
-def PostKeyEx( hwnd, key, shift, specialkey):
-
-    MakeLong = win32api.MAKELONG
-    w = win32con  # so short entry
-    ThreadId = GetWindowThreadProcessId(hwnd, None)
-
-    lparam = MakeLong(0, MapVirtualKeyA(key, 0))
-    msg_down=w.WM_KEYDOWN
-    msg_up=w.WM_KEYUP
-
-    if specialkey:
-        lparam = lparam | 0x1000000
-
-    if len(shift) > 0: #If there are modifiers - use PostMessage and AttachThreadInput
-        pKeyBuffers = PBYTE256()
-        pKeyBuffers_old = PBYTE256()
-
-    SendMessage(hwnd, w.WM_ACTIVATE, w.WA_ACTIVE, 0)
-    AttachThreadInput(GetCurrentThreadId(), ThreadId, True)
-    GetKeyboardState( ctypes.byref(pKeyBuffers_old ))
-
-    for modkey in shift:
-        if modkey == w.VK_MENU:
-            lparam = lparam | 0x20000000
-            msg_down=w.WM_SYSKEYDOWN
-            msg_up=w.WM_SYSKEYUP
-            pKeyBuffers[modkey] |= 128
-
-
-    SetKeyboardState( ctypes.byref(pKeyBuffers) )
-    time.sleep(0.01)
-    PostMessage( hwnd, msg_down, key, lparam)
-    time.sleep(0.01)
-    PostMessage( hwnd, msg_up, key, lparam | 0xC0000000)
-    time.sleep(0.01)
-    SetKeyboardState( ctypes.byref(pKeyBuffers_old) )
-    time.sleep(0.01)
-    AttachThreadInput(GetCurrentThreadId(), ThreadId, False)
-
+import xmltodict
 
 
 # #打开剪贴板
@@ -108,17 +53,40 @@ text = array('b', b'\x00\x00' * 99)
 text_len = win32gui.SendMessage(handle, win32con.WM_GETTEXT, 99, text)
 win32gui.PyGetString(text.buffer_info()[0], 99 - 1).lower()
 
-win32gui.PostMessage(handle, win32con.WM_CHAR, ord('a'), 0xF0001)
-
-win32gui.PostMessage(handle, win32con.WM_KEYDOWN, win32con.VK_CONTROL, 0xF0001)
-win32gui.PostMessage(handle, win32con.WM_KEYUP, ord('v'), 0xF0001)
-
-win32gui.SendMessage(handle, win32con.WM_KEYDOWN, 0x00000011, 0x401D0001)
+win32gui.SendMessage(handle, win32con.WM_ACTIVATE, 0, 0)
+win32api.keybd_event(win32con.VK_CONTROL,0,0,0);
+time.sleep(1)
 win32gui.SendMessage(handle, win32con.WM_KEYDOWN, 0x00000041, 0x401E0001)
-win32gui.PostMessage(handle, win32con.WM_KEYUP, 0x00000041, 0x401E0001)
-win32gui.PostMessage(handle, win32con.WM_KEYUP, 0x00000011, 0x401D0001)
-win32gui.PostMessage(handle, win32con.WM_CHAR, 0x00000001, 0x401E0001)
+win32gui.SendMessage(handle, win32con.WM_CHAR, 0x00000001, 0x001E0001)
+time.sleep(1)
+win32gui.SendMessage(handle, win32con.WM_KEYDOWN, 0x00000043, 0x002E0001)
+win32gui.SendMessage(handle, win32con.WM_CHAR, 0x00000003, 0x002E0001)
+time.sleep(1)
+win32api.keybd_event(win32con.VK_CONTROL,0,win32con.KEYEVENTF_KEYUP,0)
+
+
+win32gui.SendMessage(handle, win32con.WM_PASTE, 0, 0)
+
+# 49860
+format=None
+w.OpenClipboard()
+# if format is None:
+#     format = w.EnumClipboardFormats()
+# else:
+#     format = w.EnumClipboardFormats(format)
+data = w.GetClipboardData(49860)
+print(format)
+print(data)
+w.CloseClipboard()
+
+
+ele = xmltodict.parse(data.decode('utf-8'))
+for item in ele['QQRichEditFormat']['EditElement']:
+    print("key:",item.keys())
+    # print("item:",item)
+    print(item['@type'])
+    # if int(item['@type']) == 0 and '#text' in item.keys():
+    #     print(item['#text'])
 
 
 
-PostKeyEx(handle,ord('A'),[win32con.VK_CONTROL],False)
