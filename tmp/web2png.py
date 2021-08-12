06b852aa782e1d5ec5b0a610931979b65106df7d
cairn <kai_kai03@hotmail.com>
2021-08-12 17:16:59 +0800

    ~

--- tmp/web2png.py
+++ tmp/web2png.py
@@ -18,7 +18,7 @@ def webshot(tup):
     link = tup[1]
     print(link)
 
-    driver.get('https://xx.cn/view/login.jsp')
+    driver.get('https://test.drims.cn/view/login.jsp')
     driver.implicitly_wait(10)
     print("login:")
     input_account = driver.find_element_by_id('login')
@@ -61,11 +61,67 @@ def webshot(tup):
     except Exception as e:
         print(picname, e)
 
+def print_test():
+    print("当前进程%d已启动" % os.getpid())
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+
+    driver.get('https://test.drims.cn/view/login.jsp')
+    while driver.execute_script("return document.getElementById('login')?false:true"):
+        print('wait')
+        time.sleep(0.1)
+
+
+    print("login:")
+    input_account = driver.find_element_by_id('login')
+    input_account.send_keys('1004')
+    print("password:")
+    input_pwd = driver.find_element_by_id('password')
+    input_pwd.send_keys('123456a')
+
+    cookie = [item['name'] + '=' + item['value'] for item in driver.get_cookies()]
+    print("start:",cookie)
+
+    driver.execute_script("return document.getElementById('login')")
+
+
+    button_login = driver.find_element_by_css_selector("[class='btn btn-block btn-success']")
+    # button_login = driver.find_element_by_link_text('登录')
+    button_login.click()
+
+    cookie = [item['name'] + '=' + item['value'] for item in driver.get_cookies()]
+    print("get:",cookie)
+
+
+    driver.get('https://test.drims.cn/view/patient/patientDetail.jsp?id=368&type=1')
+    while driver.execute_script("return typeof detailToday==='function'?detailToday('A368', 'rd_scale_physical', 309, '体格生长', 4313, '2021-08-05 15:43:55', 2119):true"):
+        print('step1 wait')
+        time.sleep(0.1)
+    while driver.execute_script("return typeof printScale==='function'?printScale():true"):
+        print('print window wait')
+        time.sleep(0.1)
+
+    while not driver.execute_script("return $('#popup_ok').length>0"):
+        print('popup_ok wait')
+        time.sleep(0.1)
+    driver.execute_script("$('#popup_ok').trigger('click')")
+
+    time.sleep(10)
+    driver.quit()
+
 
 if __name__ == '__main__':
-    filename = "e:/phantomjstmp/pics/"
-    if not os.path.isdir(filename):
-        os.makedirs(filename)
+    print_test()
+
+
+    # filename = "e:/phantomjstmp/pics/"
+    # if not os.path.isdir(filename):
+    #     os.makedirs(filename)
 
     # 读取保存url的文件，返回一个列表
     # 列表中每个元素都是一个元组，文件保存url的格式是：保存为图片的名称, 网页地址。
@@ -87,7 +143,7 @@ if __name__ == '__main__':
     # pool.join()
 
     # webshot(('baidu.png','https://www.baidu.com'))
-    webshot(('drims_conner.png', 'https://xx.cn/view/preciousBaby/scale/rd_scale_conners.jsp?support=print&tableName=rd_scale_conners&tableDesc=Conners（父母问卷）&itemId=28&detailId=4023&printReporter=吕&printReporterId=3&printSign=8b3de1aa14e8453284f96b8333e01c9c&sendId=1853&visNo=A368&growPrint=undefined'))
-    # webshot(('drims_zaoqi.png','https://xx.cn/view/preciousBaby/scale/rd_scale_rainbow.jsp?support=print&tableName=rd_scale_rainbow&tableDesc=%E5%84%BF%E7%AB%A5%E6%97%A9%E6%9C%9F%E5%8F%91%E5%B1%95%E8%AF%84%E4%BC%B0%E4%B8%8E%E5%85%BB%E8%82%B2%E6%8C%87%E5%AF%BC&itemId=1702&detailId=3864&printReporter=%E9%AB%98%E4%BD%93%E9%AA%8C&printReporterId=20&printSign=9b177b7e3faf4f2098652546d2d6d486&sendId=&visNo=A355&growPrint=undefined'))
+    # webshot(('drims_conner.png', 'https://test.drims.cn/view/preciousBaby/scale/rd_scale_conners.jsp?support=print&tableName=rd_scale_conners&tableDesc=Conners（父母问卷）&itemId=28&detailId=4023&printReporter=吕&printReporterId=3&printSign=8b3de1aa14e8453284f96b8333e01c9c&sendId=1853&visNo=A368&growPrint=undefined'))
+    # webshot(('drims_zaoqi.png','https://test.drims.cn/view/preciousBaby/scale/rd_scale_rainbow.jsp?support=print&tableName=rd_scale_rainbow&tableDesc=%E5%84%BF%E7%AB%A5%E6%97%A9%E6%9C%9F%E5%8F%91%E5%B1%95%E8%AF%84%E4%BC%B0%E4%B8%8E%E5%85%BB%E8%82%B2%E6%8C%87%E5%AF%BC&itemId=1702&detailId=3864&printReporter=%E9%AB%98%E4%BD%93%E9%AA%8C&printReporterId=20&printSign=9b177b7e3faf4f2098652546d2d6d486&sendId=&visNo=A355&growPrint=undefined'))
     # webshot(('drims_zaoqi.png',''))
 
